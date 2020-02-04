072195dd6258d55c2bb788cc207d9588984c9f1c
Matias <Mssunde1999@gmail.com>
2020-02-04 14:54:19 +0100

    Lagde nytt map

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -11,8 +11,8 @@ public class Main {
     public static void main(String[] args) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
         cfg.title = "hello-world";
-        cfg.width = 480;
-        cfg.height = 320;
+        cfg.width = 500;
+        cfg.height = 500;
 
 
         GameMap<ITile> map = new GameMap<>(10, 10);
