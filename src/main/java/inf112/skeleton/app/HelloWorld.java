938b845424ccfdd78d1df0fc10c152ca7b79dded
Hans-Christian Siebke <hsi015@uib.no>
2020-02-06 12:42:33 +0100

    camera & render

--- src/main/java/inf112/skeleton/app/HelloWorld.java
+++ src/main/java/inf112/skeleton/app/HelloWorld.java
@@ -4,11 +4,13 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL30;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
 import com.badlogic.gdx.scenes.scene2d.ui.Cell;
 
 public class HelloWorld implements ApplicationListener {
@@ -19,6 +21,8 @@ public class HelloWorld implements ApplicationListener {
     TiledMapTileLayer hole;
     TiledMapTileLayer wall;
     TiledMapTileLayer flag;
+    OrthoCachedTiledMapRenderer render;
+    OrthographicCamera camera;
 
 
     @Override
@@ -33,6 +37,11 @@ public class HelloWorld implements ApplicationListener {
         wall = (TiledMapTileLayer) new TiledMap().getLayers().get("Walls");
         flag = (TiledMapTileLayer) new TiledMap().getLayers().get("Flags");
 
+        camera.setToOrtho(false,3.0f,5);
+        camera.update();
+
+        render.setView(camera);
+
     }
 
     @Override
@@ -45,11 +54,6 @@ public class HelloWorld implements ApplicationListener {
     public void render() {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
-
-        batch.begin();
-        font.draw(batch, "Hello World", 200, 200);
-        font.draw(batch, "Goodbye World", 200, 50);
-        batch.end();
     }
 
     @Override
