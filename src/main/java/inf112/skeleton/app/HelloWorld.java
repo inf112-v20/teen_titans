336b19daccfb1b5ab4de1edb93c193b5f0a5c164
Jostein <jph001@uib.no>
2020-02-06 13:56:30 +0100

    La inn kamera, får error

--- src/main/java/inf112/skeleton/app/HelloWorld.java
+++ src/main/java/inf112/skeleton/app/HelloWorld.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL30;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -12,6 +13,8 @@ import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
@@ -25,6 +28,8 @@ public class HelloWorld implements ApplicationListener {
     TiledMapTileLayer wall;
     TiledMapTileLayer flag;
     TiledMapTileLayer playerLayer;
+    OrthogonalTiledMapRenderer renderer;
+    OrthographicCamera camera;
 
     Vector2 position;
     Cell playerCell;
@@ -37,7 +42,10 @@ public class HelloWorld implements ApplicationListener {
         font = new BitmapFont();
         font.setColor(Color.RED);
 
+
         map = new TmxMapLoader().load("assets/testMap.tmx");
+        camera = new OrthographicCamera();
+        renderer = new OrthogonalTiledMapRenderer(map, 1/90000);
         ground = (TiledMapTileLayer) new TiledMap().getLayers().get("Ground");
         hole = (TiledMapTileLayer) new TiledMap().getLayers().get("Holes");
         wall = (TiledMapTileLayer) new TiledMap().getLayers().get("Walls");
@@ -45,11 +53,16 @@ public class HelloWorld implements ApplicationListener {
         playerLayer = (TiledMapTileLayer) new TiledMap().getLayers().get("Player");
 
         Texture player = new Texture(Gdx.files.internal("player.png"));
-        TextureRegion[][] frank = new TextureRegion(new Texture(Gdx.files.internal("player.png"))).split(300,300);
+        TextureRegion[][] frank = new TextureRegion(new Texture("player.png")).split(300,300);
         playerCell.setTile(new StaticTiledMapTile(frank[0][0]));
         playerDead.setTile(new StaticTiledMapTile(frank[0][1]));
         playerWon.setTile(new StaticTiledMapTile(frank[0][2]));
 
+        camera.setToOrtho(false, 8, 8);
+        camera.viewportWidth = 8;
+        camera.viewportHeight = 8;
+        camera.position.set(8/2f, 8/2f, 0);
+        renderer.setView(camera);
 
     }
 
@@ -64,10 +77,7 @@ public class HelloWorld implements ApplicationListener {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
 
-        batch.begin();
-        font.draw(batch, "Hello World", 200, 200);
-        font.draw(batch, "Goodbye World", 200, 50);
-        batch.end();
+        renderer.render();
 
         playerLayer.setCell(0, 2400, playerCell);
 
