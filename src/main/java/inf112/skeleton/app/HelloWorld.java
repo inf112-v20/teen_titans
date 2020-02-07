dc5007dc939d6076a4361511715ffac3b4f7630e
bendik.solevag@student.uib.no <Bendik.Solevag@student.uib.no>
2020-02-07 11:13:59 +0100

    Improved keyUp method, added functionality to check if position is valid.

--- src/main/java/inf112/skeleton/app/HelloWorld.java
+++ src/main/java/inf112/skeleton/app/HelloWorld.java
@@ -107,30 +107,40 @@ public class HelloWorld extends InputAdapter implements ApplicationListener {
 
     @Override
     public boolean keyUp(int keycode){
-        switch(keycode){
-            case(Input.Keys.UP):
-                playerLayer.setCell((int) position.x, (int) position.y, null);
-                position.y -= 300;
-                playerLayer.setCell((int) position.x, (int) position.y, playerCell);
-                return true;
-
-            case(Input.Keys.DOWN):
-                playerLayer.setCell((int) position.x, (int) position.y, null);
-                position.y += 300;
-                playerLayer.setCell((int) position.x, (int) position.y, playerCell);
-                return true;
-            case(Input.Keys.RIGHT):
-                playerLayer.setCell((int) position.x, (int) position.y, null);
-                position.x += 300;
-                playerLayer.setCell((int) position.x, (int) position.y, playerCell);
-                return true;
-            case(Input.Keys.LEFT):
-                playerLayer.setCell((int) position.x, (int) position.y, null);
-                position.x -= 300;
-                playerLayer.setCell((int) position.x, (int) position.y, playerCell);
-                return true;
+        Vector2 newPos = generateNewPlayerPosition(keycode);
+        if(validPlayerPosition(newPos)){
+            playerLayer.setCell((int)position.x, (int)position.y, null);
+            position = newPos;
+            playerLayer.setCell((int)position.x, (int)position.y, playerCell);
+            return true;
         }
         return false;
     }
+
+    /**
+     * @param keycode key that was pressed
+     * @return a new position if player has pressed a key indicating a move, old position otherwise.
+     */
+    public Vector2 generateNewPlayerPosition(int keycode) {
+        if(keycode == Input.Keys.UP) return new Vector2().set(position.x, position.y+1);
+        else if(keycode == Input.Keys.DOWN) return new Vector2().set(position.x, position.y-1);
+        else if(keycode == Input.Keys.LEFT) return new Vector2().set(position.x-1, position.y);
+        else if(keycode == Input.Keys.RIGHT) return new Vector2().set(position.x+1, position.y);
+        else return position;
+
+    }
+
+    /**
+     * Checks whether suggested player position is valid.
+     * @param pos new position to try.
+     * @return true if given position is a valid plauyer position, false otherwise.
+     */
+    public boolean validPlayerPosition(Vector2 pos){
+        if(wall.getCell((int)pos.x,(int)pos.y) != null || pos.x < 0 || pos.x >= camera.viewportWidth || pos.y < 0 || pos.y >= camera.viewportHeight){
+            return false;
+        }
+        return true;
+    }
+
 
 }
