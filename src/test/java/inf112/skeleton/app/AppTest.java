a8b61400deffaa31e15a488f13fab2ae43c87b7c
Hans-Christian Siebke <hsi015@uib.no>
2020-02-21 11:50:27 +0100

    notater for hva vi skal lage tester av

--- src/test/java/inf112/skeleton/app/AppTest.java
+++ src/test/java/inf112/skeleton/app/AppTest.java
@@ -6,11 +6,29 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
 {
     /**
      * Rigorous Test :-)
      */
+
+    ///player position test
+
+    ///player hp test (hvis han tar 1 damage orginal hp -1)
+
+    ///game map test
+
+    ///player dead, true hvis lever, false død
+
+    ///player move, hvis han står i en posisjon så keycode, ny posisjon(keycode)
+
+    ///player damage
+
+    ///illegal moves
+
+
+
+
     @Test
     public void shouldAnswerWithTrue()
     {
