46a2539bdbf4971a041b240867cf4ceda6a4c4ed
bendik.solevag@student.uib.no <Bendik.Solevag@student.uib.no>
2020-02-27 11:40:14 +0100

    Added 2nd thread to handle game logic.

--- src/main/java/inf112/skeleton/app/Renderer.java
+++ src/main/java/inf112/skeleton/app/Renderer.java
@@ -20,19 +20,19 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Renderer implements ApplicationListener {
-
     private GameLoop gameLoop;
     private OrthogonalTiledMapRenderer renderer;
     private OrthographicCamera camera;
-    private int count;
-
-    public Renderer(){
-        count = 0;
-    }
 
     @Override
     public void create() {
-        gameLoop = new GameLoop();
+        try {
+            gameLoop = new GameLoop();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        gameLoop.loop.start();
+
         Gdx.input.setInputProcessor(gameLoop.getBoard());
         camera = new OrthographicCamera();
         renderer = new OrthogonalTiledMapRenderer(gameLoop.getBoard().getMap(), 1/300f);
@@ -54,7 +54,6 @@ public class Renderer implements ApplicationListener {
     public void render() {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
-        if(count++ % 30 == 0) gameLoop.loop();
         renderer.render();
 
     }
