09b9b33bd187c3c0fd98f3a3f4d0ba8e54cf0509
Hans-Christian Siebke <hsi015@uib.no>
2020-02-28 10:52:19 +0100

    laget tetster

--- src/test/java/inf112/skeleton/app/AppTest.java
+++ src/test/java/inf112/skeleton/app/AppTest.java
@@ -1,17 +1,29 @@
 package inf112.skeleton.app;
 
 import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest
-{
+
+public class AppTest {
+
+    @Before
+    public void setUp(){
+        Board game = new Board();
+    }
+
+
+
     /**
      * Rigorous Test :-)
      */
 
+
+    
     ///player position test
 
     ///player hp test (hvis han tar 1 damage orginal hp -1)
