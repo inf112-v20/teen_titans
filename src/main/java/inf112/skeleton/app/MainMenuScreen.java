f7565b3fb720123286d4702cf0e5b46122a479cb
Ola Tønnes Straum <olatonnes@gmail.com>
2020-03-24 21:34:02 +0100

    Start på main menu

---
+++ src/main/java/inf112/skeleton/app/MainMenuScreen.java
@@ -0,0 +1,40 @@
+package inf112.skeleton.app;
+
+import com.badlogic.gdx.Screen;
+
+public class MainMenuScreen implements Screen {
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float v) {
+
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
