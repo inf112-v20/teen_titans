3c96795e224240c766a7ce5cfbdc26be3a12e28d
OlaBola98 <olatonnes@gmail.com>
2020-03-26 22:25:44 +0100

    MenuScreen skal være ferdig, men tror den ligger bak Spillbrettet

--- src/main/java/inf112/skeleton/app/scenes/MainMenuScreen.java
+++ src/main/java/inf112/skeleton/app/scenes/MainMenuScreen.java
@@ -1,28 +1,32 @@
 package inf112.skeleton.app.scenes;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.utils.Predicate;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class MainMenuScreen implements Screen {
-private Orchestrator parent;
-    public MainMenuScreen() {
-        Orchestrator orchestrator = null;
+    private Orchestrator parent;
+    private Stage stage;
+
+    public MainMenuScreen(Orchestrator orchestrator) {
         parent = orchestrator;
-        Stage stage = new Stage(new ScreenViewport());
+        stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
-        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1/30f));
-        stage.draw();
+    }
+
+    @Override
+    public void show(){
         Table table = new Table();
         table.setFillParent(true);
         table.setDebug(true);
-        table.addActor(table);
+        stage.addActor(table);
 
         Skin skin = new Skin(Gdx.files.internal("skin/glassy-ui.json"));
 
@@ -35,20 +39,42 @@ private Orchestrator parent;
         table.add(preferences).fillX().uniformX();
         table.row();
         table.add(exit).fillX().uniformX();
-    }
 
-    @Override
-    public void show() {
+        exit.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent changeEvent, Actor actor) {
+                Gdx.app.exit();
+            }
+        });
 
+        newGame.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent changeEvent, Actor actor) {
+                parent.changeScreens(Orchestrator.APPLICATION);
+            }
+        });
+
+        preferences.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent changeEvent, Actor actor) {
+                parent.changeScreens(Orchestrator.PREFERENCES);
+            }
+        });
     }
 
     @Override
     public void render(float v) {
+        Gdx.gl.glClearColor(0f,0f,0f,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(),1/30f));
+        stage.draw();
 
     }
 
     @Override
-    public void resize(int i, int i1) {
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
 
     }
 
@@ -69,6 +95,6 @@ private Orchestrator parent;
 
     @Override
     public void dispose() {
-
+        stage.dispose();
     }
 }
