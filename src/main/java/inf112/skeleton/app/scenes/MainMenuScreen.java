52a39b3ddc2d2b80bc96abc6e5869096fb5ea632
OlaBola98 <olatonnes@gmail.com>
2020-03-26 20:46:28 +0100

    Fortsettelse på menuscreen

--- src/main/java/inf112/skeleton/app/scenes/MainMenuScreen.java
+++ src/main/java/inf112/skeleton/app/scenes/MainMenuScreen.java
@@ -1,8 +1,42 @@
 package inf112.skeleton.app.scenes;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.Predicate;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class MainMenuScreen implements Screen {
+private Orchestrator parent;
+    public MainMenuScreen() {
+        Orchestrator orchestrator = null;
+        parent = orchestrator;
+        Stage stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1/30f));
+        stage.draw();
+        Table table = new Table();
+        table.setFillParent(true);
+        table.setDebug(true);
+        table.addActor(table);
+
+        Skin skin = new Skin(Gdx.files.internal("skin/glassy-ui.json"));
+
+        TextButton newGame = new TextButton("New Game", skin);
+        TextButton preferences = new TextButton("Preferences", skin);
+        TextButton exit = new TextButton("Exit", skin);
+
+        table.add(newGame).fillX().uniformX();
+        table.row().pad(10,0,0,0);
+        table.add(preferences).fillX().uniformX();
+        table.row();
+        table.add(exit).fillX().uniformX();
+    }
+
     @Override
     public void show() {
 
