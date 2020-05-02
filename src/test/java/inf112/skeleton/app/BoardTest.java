e0fe1b91197e601f902403d81c9861be0605c08a
Baynrich <bendikasolevaag@gmail.com>
2020-05-02 19:50:10 +0200

    Added setup to boardTest class

--- src/test/java/inf112/skeleton/app/BoardTest.java
+++ src/test/java/inf112/skeleton/app/BoardTest.java
@@ -1,6 +1,12 @@
 package inf112.skeleton.app;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
+import com.badlogic.gdx.graphics.GL20;
 import inf112.skeleton.app.board.Board;
 import inf112.skeleton.app.board.Pos;
 import org.junit.Before;
@@ -11,6 +17,9 @@ public class BoardTest {
 
     @Before
     public void setUp(){
+        HeadlessApplicationConfiguration conf = new HeadlessApplicationConfiguration();
+        new HeadlessApplication(new TestpurposesApplicationListener(), conf);
+        Gdx.gl = mock(GL20.class);
         testBoard = new Board();
     }
 
